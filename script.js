52d717ec0a98ea214341fdb9efc602c53b36e19b
Eduardo O <viverlin7@gmail.com>
2023-06-20 12:10:14 -0600

    complete merge arr

--- script.js
+++ script.js
@@ -1587,11 +1587,13 @@ var merge = function(nums1, m, nums2, n) {
     let k = m + n - 1;
 
     while (j >= 0) {
-        
+        if (i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
     }
 
 
-
-    return nums1.sort((a,b) => a - b)
 
 };
